efe28a229bb605d51a02e80ff47a9a3d7db54749
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-10 17:03:02 -0600

    end of class

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -6,10 +6,41 @@ const itunesService = new ItunesService()
 
 function drawSongs(results) {
   console.log(results)
-  //YOUR CODING STARTS HERE
-  
-
-
+  let song = results[0];
+  let template1 = `
+  <div class="col-sm-8 h-75">
+  <div class="card">
+    <h2 class="card-title">${song.title}</h2>
+    <img class="card-img-top" src="${song.albumArt}" alt="ablum art">
+    <div class="card-body">
+       <h4 class="card-subtitle">${song.artist}</h4>
+       <h3 class="card-subtitle">${song.collection}</h3>
+       <audio controls>
+          <source src="${song.preview}">
+       </audio>
+       <h2>$${song.price}</h2>
+    </div>
+  </div>
+  </div>  
+  `
+  let template = '';
+  for (let i = 1; i < results.length; i++) {
+    const song = results[i];
+    template += `
+    <div class="card">
+    <h2 class="card-title">${song.title}</h2>
+    <img class="card-img-top" src="${song.albumArt}" alt="ablum art">
+    <div class="card-body">
+       <h4 class="card-subtitle">${song.artist}</h4>
+       <h3 class="card-subtitle">${song.collection}</h3>
+       <span>${song.preview}</span>
+       <h2>$${song.price}</h2>
+    </div>
+  </div>  
+  `
+  }
+  document.querySelector(".top-hit").innerHTML = template1;
+  document.getElementById("songs").innerHTML = template;
 }
 
 
--- app/models/Song.js
+++ app/models/Song.js
@@ -1,7 +1,7 @@
 export default class Song {
     constructor(song) {
         this.title = song.trackName
-        this.albumArt = song.artworkUrl60.replace(/60x60/g, "300x300")
+        this.albumArt = song.artworkUrl60.replace(/60x60/g, "500x500")
         this.artist = song.artistName
         this.collection = song.collectionName
         this.price = song.collectionPrice
